9aed8fcfc9edfad3283fc134cf4527b159cde50f
Simon Drobinski <simon.drobinski@hotmail.fr>
2021-10-22 23:38:45 +0200

    feature: created Task model, made migrations with db and created super user in admin to manipulate Task app

--- backend/todolist/tasks/models.py
+++ backend/todolist/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    TASK_DONE = [
+        ('done', 'DONE'),
+        ('not done', 'NOT DONE')
+    ]
+    content = models.CharField(max_length=200)
+    completion = models.BooleanField(choices=TASK_DONE)
+
+    def __str__(self):
+        return self.content
+
