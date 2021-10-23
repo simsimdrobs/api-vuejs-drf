2097090dde905e14119ace98bf7cf98baf2661df
Simon Drobinski <simon.drobinski@hotmail.fr>
2021-10-24 00:02:58 +0200

    feature: defined our router and the routes we will use to call our Task__.vue files

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import TaskList from '../views/TaskList'
+import TaskDetail from '../views/TaskDetail'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    alias: '/tasks',
+    name: 'task-list',
+    component: TaskList
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/tasks/:id',
+    name: 'task-detail',
+    component: TaskDetail
+  },
 ]
 
 const router = createRouter({
